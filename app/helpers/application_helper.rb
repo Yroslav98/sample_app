a5395cdbc5430176d11861d38ca14028bc57e744
Yroslav98 <yroslaavvv@gmail.com>
2023-05-28 13:38:29 +0000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Возвращает полное название для каждой страницы.   # Строка комментария 
+  def full_title(page_title = '')                     # Определение метода и необязательный аргумент
+    base_title = "Ruby on Rails Tutorial Sample App"  # Присвоение переменной
+    if page_title.empty?                              # Логический тест 
+      base_title                                      # Неявное возвращаемое значение
+    else
+      "#{page_title} | #{base_title}"                 # Строковое выражение 
+    end
+  end
 end
